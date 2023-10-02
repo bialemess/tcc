a67830f842bfbc5e782dcc9c0385cb61a2f43d6a
Bianca Lemes Costa <112573642+bialemess@users.noreply.github.com>
2023-10-02 16:53:44 -0300

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,12 +1,12 @@
 <?php
 include("conexaoBD.php");
 
-if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-    $nome = isset($_POST["nome"]) ? $_POST["nome"] : "";
+if ($_SERVER["REQUEST_METHOD"] === 'POST' && isset($_POST["code"])) {
+    $code = $_POST["code"];
 
     try {
-        $stmt = $pdo->prepare("DELETE FROM produtoTCC WHERE nome = :nome");
-        $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
+        $stmt = $pdo->prepare("DELETE FROM produtoTCC WHERE code = :code");
+        $stmt->bindValue(':code', $code);
         $stmt->execute();
     } catch (PDOException $e) {
         echo 'Error: ' . $e->getMessage();
